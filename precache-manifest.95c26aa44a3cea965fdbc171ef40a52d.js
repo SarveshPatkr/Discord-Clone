967a257d82a39ee59dca38fc06dc25ee31213975
Sarvesh Patkar <spatkar131201@gmail.com>
2021-06-24 19:08:15 +0530

    Updates

---
+++ precache-manifest.95c26aa44a3cea965fdbc171ef40a52d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3debf8e8d4339c3324ecc8464052aa",
+    "url": "/Discord-Clone/index.html"
+  },
+  {
+    "revision": "395c90dfe8889aff772c",
+    "url": "/Discord-Clone/static/css/main.bc012f8a.chunk.css"
+  },
+  {
+    "revision": "d1763662eb1ada292dd2",
+    "url": "/Discord-Clone/static/js/2.ad55f908.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/Discord-Clone/static/js/2.ad55f908.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "395c90dfe8889aff772c",
+    "url": "/Discord-Clone/static/js/main.27411e50.chunk.js"
+  },
+  {
+    "revision": "9b67da8e732c7a75f11d",
+    "url": "/Discord-Clone/static/js/runtime-main.c1502df8.js"
+  }
+]);
